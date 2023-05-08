3a730a9766e088d1f631f8884c17c3c58a012ce6
abhinav-3007 <abhinavgoyal2006@gmail.com>
2023-05-08 19:11:07 +0530

    First Commit adding all files to git

---
+++ Calculator/src/Abhinav_11A_2_Project.java
@@ -0,0 +1,9 @@
+// Class to run Calculator class and call methods
+class Run {
+    public static void main(String[] args)
+    {
+        Calculator calc = new Calculator();
+        calc.setUp();
+        calc.setUpButtonListeners();
+    }
+}
